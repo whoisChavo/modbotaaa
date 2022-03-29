b9676f54c1a85c82750ade287e999885fdb44e99
Chavo but Happy <89283086+whoisChavo@users.noreply.github.com>
2022-03-29 15:07:14 +0300

    Update say.js

--- komutlar/say.js
+++ komutlar/say.js
@@ -4,8 +4,11 @@ exports.run = async (client, message, args) => {
   if(!message.member.roles.cache.has(ayarlar.registerhammer) && !message.member.hasPermission("ADMINISTRATOR")) return message.react(ayarlar.red)
   var bot = message.guild.members.cache.filter(s => s.voice.channel && s.user.bot).size
    var tag = message.guild.members.cache.filter(m => m.user.username.includes("ᛠ")).size 
+   var tag1 = message.guild.members.cache.filter(m => m.user.username.includes("vienna")).size 
+   var tag2 = message.guild.members.cache.filter(m => m.user.username.includes("Vienna")).size 
   var etiket =  message.guild.members.cache.filter(s => !s.bot).filter(member => member.user.discriminator == "").size;
     var taglı = message.guild.members.cache.filter(s => !s.bot).filter(member => member.roles.cache.get("957599853674921994")).size; 
+    let tagtotal = tag + tag2;
         var toplamüye = message.guild.memberCount
         var online = message.guild.members.cache.filter(off => off.presence.status !== 'offline').size
         var Sesli = message.guild.members.cache.filter(s => s.voice.channel).size;
@@ -14,7 +17,7 @@ exports.run = async (client, message, args) => {
         message.lineReply(new MessageEmbed()
             .setDescription(`\`❯\` Toplam \**${toplamüye}\** kişi Vienna'nın Kanatları Altında.
             \`❯\` Seste toplam \**${Sesli}\** Üye Bulunmakta.
-            \`❯\` Toplam \**${tag}\** kişi ailemize katılmış!
+            \`❯\` Toplam \**${tagtotal}\** kişi ailemize katılmış!
             \`❯\` Sunucumuzda Toplam \**${boost}\** Boost Bulunmakta (**${boostlevel}.seviye**).
 `) 
             )
